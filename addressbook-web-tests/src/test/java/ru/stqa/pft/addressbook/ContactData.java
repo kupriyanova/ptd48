9e6564d051dd444525141f666e2f95703288d5b8
Ekaterina Kupriyanova <ek.kupriyanova@gmail.com>
2018-01-14 19:24:01 +0300

    Create group and contact tests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String name;
+    private final String midlename;
+    private final String lastname;
+    private final String mobile;
+    private final String email;
+
+    public ContactData(String name, String midlename, String lastname, String mobile, String email) {
+        this.name = name;
+        this.midlename = midlename;
+        this.lastname = lastname;
+        this.mobile = mobile;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMidlename() {
+        return midlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
